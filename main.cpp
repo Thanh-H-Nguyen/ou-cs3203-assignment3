a90a588d4ec91eef6da2c620c969a9b5e782103b
Thanh-H-Nguyen <125384505+Thanh-H-Nguyen@users.noreply.github.com>
2023-02-17 23:35:04 -0600

    Add to your main method

--- main.cpp
+++ main.cpp
@@ -2,6 +2,22 @@
 #include <list>
 using namespace std;
 
+int Sum(list<int> numbers){
+    int sum = 0;
+    for(int i: numbers){
+        sum = sum + i;
+    }
+    return sum;
+}
+
+int Multiplies(list<int> numbers){
+    int mult = 1;
+    for(int i : numbers){
+        mult = mult * i;
+    }
+    return mult;
+}
+
 list<int> Reverse(list<int> numbers) {
     numbers.reverse();
     return numbers;
@@ -12,9 +28,13 @@ int main(){
     int num;
     cout << "Enter numbers: ";
     while(cin >> num){
-        numbers.push_back(num);// to add a new number to the end of list
+        numbers.push_back(num);
         cout << "Enter numbers: ";
     }
+    cout << endl;
+    cout <<"Sum: " << Sum(numbers) << endl;
+    cout <<"Mult: " << Multiplies(numbers) << endl;
+    
     list<int> reversedNumbers = Reverse(numbers);
     cout << "Reversed: ";
     for (int i : reversedNumbers) {
@@ -22,4 +42,5 @@ int main(){
     }
     cout << endl;
     return 0;
+
 }
