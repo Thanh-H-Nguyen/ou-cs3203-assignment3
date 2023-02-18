4101ecf83f1c2e36c06a59053f3a969ce61903f0
Thanh-H-Nguyen <125384505+Thanh-H-Nguyen@users.noreply.github.com>
2023-02-17 23:42:18 -0600

    Add some comments, part10

--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include <list>
 using namespace std;
 
+// sum of a list
 int Sum(list<int> numbers){
     int sum = 0;
     for(int i: numbers){
@@ -9,7 +10,7 @@ int Sum(list<int> numbers){
     }
     return sum;
 }
-
+//computes the product (multiplication) of all of a list
 int Multiplies(list<int> numbers){
     int mult = 1;
     for(int i : numbers){
@@ -18,23 +19,25 @@ int Multiplies(list<int> numbers){
     return mult;
 }
 
+//reverse order
 list<int> Reverse(list<int> numbers) {
     numbers.reverse();
     return numbers;
 }
 
 int main(){
+    //create a list
     list<int> numbers;
     int num;
     cout << "Enter numbers: ";
     while(cin >> num){
-        numbers.push_back(num);
+        numbers.push_back(num);// to add a new number to the end of list
         cout << "Enter numbers: ";
     }
     cout << endl;
     cout <<"Sum: " << Sum(numbers) << endl;
     cout <<"Mult: " << Multiplies(numbers) << endl;
-    
+    // call reversedNumbers to passing from numbers
     list<int> reversedNumbers = Reverse(numbers);
     cout << "Reversed: ";
     for (int i : reversedNumbers) {
